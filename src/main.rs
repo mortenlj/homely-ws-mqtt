4c2925dc36531c3e65c1442c7f8a57b4a325f388
Morten Lied Johansen <mortenjo@ifi.uio.no>
2022-06-27 21:29:11 +0200

    Perform auth request

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,26 @@
 extern crate log;
 
 use std::cmp::min;
+use std::collections::HashMap;
 
 use anyhow::{anyhow, Result};
 use async_ctrlc::CtrlC;
 use async_std::prelude::FutureExt;
-use env_logger::Env;
-use tide::Request;
 use clap::Parser;
+use env_logger::Env;
+use serde::Deserialize;
+use tide::Request;
+
+const AUTH_URL: &'static str = "https://sdk.iotiliti.cloud/homely/oauth/token";
+
+#[derive(Deserialize)]
+struct Auth {
+    access_token: String,
+}
 
 /// Collect events from Homely API (REST and WebSockets) and send to MQTT
-#[derive(Parser,Debug)]
-#[clap(author="Morten Lied Johansen", version, about, long_about = None)]
+#[derive(Parser, Debug)]
+#[clap(author = "Morten Lied Johansen", version, about, long_about = None)]
 struct Args {
     /// Listen address
     #[clap(short, long, default_value = "localhost:8080")]
@@ -21,10 +30,18 @@ struct Args {
     /// Control verbosity of logs. Can be repeated
     #[clap(short, long, parse(from_occurrences))]
     verbose: usize,
+
+    /// Homely username
+    #[clap(long)]
+    homely_username: String,
+
+    /// Homely password
+    #[clap(long)]
+    homely_password: String,
 }
 
 
-#[async_std::main]
+#[tokio::main]
 async fn main() -> Result<()> {
     let args: Args = Args::parse();
 
@@ -38,6 +55,8 @@ async fn main() -> Result<()> {
             .map_err(|e| anyhow!(e))
     };
 
+    let token = authenticate(args.homely_username, args.homely_password).await?;
+    debug!("{}", token.access_token);
     app.race(setup_ctrlc_handler()).await?;
 
     Ok(())
@@ -46,6 +65,23 @@ async fn main() -> Result<()> {
 async fn probe(mut _req: Request<()>) -> tide::Result {
     debug!("I'm being probed!");
     Ok(format!("I'm alive!").into())
+}
+
+
+/// Authenticate with the REST API and return token
+async fn authenticate(username: String, password: String) -> Result<Auth> {
+    let mut map = HashMap::new();
+    map.insert("username", username);
+    map.insert("password", password);
+
+    let client = reqwest::Client::new();
+    let auth = client.post(AUTH_URL)
+        .json(&map)
+        .send()
+        .await?
+        .json::<Auth>()
+        .await?;
+    Ok(auth)
 }
 
 /// ^C handler will stop the server
