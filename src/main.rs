92d0c36128344a088cf89b715cca884d80cbfdf6
Morten Lied Johansen <mortenjo@ifi.uio.no>
2022-06-30 20:33:59 +0200

    Set up and make socket.io connection

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 #[macro_use]
 extern crate log;
 
-use std::borrow::Borrow;
 use std::cmp::min;
 use std::collections::HashMap;
-use std::time::Duration;
+use std::sync::{Arc, Condvar, Mutex};
 
 use anyhow::{anyhow, Result};
 use async_ctrlc::CtrlC;
 use async_std::prelude::FutureExt;
-use async_std::task::sleep;
 use clap::Parser;
 use env_logger::Env;
+use reqwest::{Client, Url};
+use rust_socketio::{Payload, SocketBuilder};
 use serde::Deserialize;
 use serde_json::Value;
 use tide::Request;
+use tokio::task;
 
 const AUTH_URL: &'static str = "https://sdk.iotiliti.cloud/homely/oauth/token";
 const LOCATIONS_URL: &'static str = "https://sdk.iotiliti.cloud/homely/locations";
 const HOME_URL: &'static str = "https://sdk.iotiliti.cloud/homely/home";
+const SOCKET_URL: &'static str = "https://sdk.iotiliti.cloud/";
 
 #[derive(Deserialize, Debug)]
 struct Auth {
@@ -28,7 +30,6 @@ struct Auth {
 #[derive(Deserialize, Debug)]
 #[serde(rename_all="camelCase")]
 struct Location {
-    name: String,
     location_id: String
 }
 
@@ -82,21 +83,58 @@ async fn probe(mut _req: Request<()>) -> tide::Result {
 
 
 async fn consume_events(username: String, password: String) -> Result<()> {
-    let auth = authenticate(username, password).await?;
-    dbg!(auth.borrow());
-    let locations = get_locations(auth.borrow()).await?;
-    dbg!(<Vec<Location> as Borrow<[Location]>>::borrow(&locations));
+    let (auth, client) = authenticate(username, password).await?;
+    dbg!(&auth);
+    let locations = get_locations(&client, &auth).await?;
+    dbg!(&locations);
 
-    let state = get_state(auth.borrow(), &locations[0]).await?;
-    dbg!(state);
+    let state = get_state(&client, &auth, &locations[0]).await?;
+    dbg!(&state);
 
-    sleep(Duration::from_secs(20)).await;
-
-    Ok(())
+    task::spawn_blocking(move || {
+        stream_events(&auth, &locations[0])
+    }).await?
 }
 
-async fn get_state(auth: &Auth, location: &Location) -> Result<Value> {
-    let client = reqwest::Client::new();
+fn stream_events(auth: &Auth, location: &Location) -> Result<()> {
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = pair.clone();
+
+    let error_handler = move |err, _ | {
+        error!("Error in socket.io: {:#?}", err);
+        let (lock, cvar) = &*pair2;
+        let mut errored = lock.lock().unwrap();
+        *errored = true;
+        cvar.notify_one();
+    };
+
+    let event_handler = |payload, _| {
+        match payload {
+            Payload::String(str) => info!("Received: {}", str),
+            Payload::Binary(bin_data) => info!("Received bytes: {:#?}", bin_data),
+        }
+    };
+
+    let mut url = Url::parse(SOCKET_URL)?;
+    url.query_pairs_mut()
+        .clear()
+        .append_pair("locationId", location.location_id.as_str());
+    info!("Making socket.io connection to {}", url.as_str());
+    SocketBuilder::new(url.as_str())
+        .opening_header("Authorization", format!("Bearer {}", auth.access_token))
+        .on("event", event_handler)
+        .on("error", error_handler)
+        .connect()?;
+
+    let (lock, cvar) = &*pair;
+    let mut errored = lock.lock().unwrap();
+    while !*errored {
+        errored = cvar.wait(errored).unwrap();
+    }
+    Err(anyhow!("Error streaming events!"))
+}
+
+async fn get_state(client: &Client, auth: &Auth, location: &Location) -> Result<Value> {
     let state = client.get(format!("{}/{}", HOME_URL, location.location_id))
         .bearer_auth(&auth.access_token)
         .send()
@@ -106,8 +144,7 @@ async fn get_state(auth: &Auth, location: &Location) -> Result<Value> {
     Ok(state)
 }
 
-async fn get_locations(auth: &Auth) -> Result<Vec<Location>> {
-    let client = reqwest::Client::new();
+async fn get_locations(client: &Client, auth: &Auth) -> Result<Vec<Location>> {
     let locations = client.get(LOCATIONS_URL)
         .bearer_auth(&auth.access_token)
         .send()
@@ -118,7 +155,7 @@ async fn get_locations(auth: &Auth) -> Result<Vec<Location>> {
 }
 
 /// Authenticate with the REST API and return token
-async fn authenticate(username: String, password: String) -> Result<Auth> {
+async fn authenticate(username: String, password: String) -> Result<(Auth, Client)> {
     let mut map = HashMap::new();
     map.insert("username", username);
     map.insert("password", password);
@@ -130,7 +167,7 @@ async fn authenticate(username: String, password: String) -> Result<Auth> {
         .await?
         .json::<Auth>()
         .await?;
-    Ok(auth)
+    Ok((auth, client))
 }
 
 /// ^C handler will stop the server
